4922a7fa12ff50810007eb9a238451b482f7284c
Hagan McPhail <hagan.lfd36@gmail.com>
2016-07-02 13:11:18 -0500

    wip

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,18 @@
 class WelcomeController < ApplicationController
+
+  def index
+  end
+
+  def show_data
+    ap "blah_success"
+    #if params[:name].present?
+    #  message = {
+    #    name: params[:name],
+    #    email: params[:email],
+    #    message: params[:message]
+    #  }
+    #  ap message
+    #end
+
+  end
 end
