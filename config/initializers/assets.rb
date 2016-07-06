87056bbef90ed07fa4c632082411428bd982e914
Hagan McPhail <hagan.lfd36@gmail.com>
2016-07-06 15:51:06 -0500

    added more assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
+Dir.glob("#{Rails.root}/app/assets/**/").each do |path|
   Rails.application.config.assets.paths << path
 end
 
